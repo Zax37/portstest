f137f2511096018fdd27129913ab7f890f703a49
zachariasz.zacharski <zachariasz.zacharski@sperasoft.com>
2022-08-04 18:14:50 +0200

    Track and show calls

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set('json spaces', 4)
 
+var calls = [];
+
+app.get('/', function(req, res) {
+    res.json(calls);
+})
+
 app.post('/', function(req, res) {
-    console.log(req);
+    calls.push(req);
     res.text('ok');
 });
 
